f1839c651959a3f64aebb8e8b654864e400bd095
Gaspard49 <gaspard.tertrais@gmail.com>
2019-09-11 11:58:20 +0200

    active storage Avatar profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def age(dob)
-    now = Time.now.utc.to_date
-    now.year - dob.year - ((now.month > dob.month || (now.month == dob.month && now.day >= dob.day)) ? 0 : 1)
-  end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :orders
+  has_one_attached :avatar
 end
 
