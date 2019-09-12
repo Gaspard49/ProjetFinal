221bd0f0d736b6fc1c907ac202097421456ff274
adrien66 <adrien66.pub@gmail.com>
2019-09-12 17:49:16 +0200

    ca marcheeeeeeeeeeeee

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,11 @@
 class OrdersController < ApplicationController
-
     include CurrentCart
     before_action :set_cart, only: [:create]
 
     def create 
         item = Item.find(params[:format])
-        @order_item = @order.item_orders.new(item: item)
-
-        #ItemOrder.create(order_id: current_user.orders.where(status: false), item_id: params[:format])
+        @order.items << item
         redirect_to root_path
     end
-    
 end
+    
