3c21a1ea024910560c2e7a9bd205b0ca27588691
BEECECE <be.charrier@yahoo.com>
2019-09-12 22:02:03 +0200

    ca va t'etre mieux commme ca

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,14 @@
 class OrdersController < ApplicationController
     include CurrentCart
-    before_action :set_cart, only: [:create]
+    before_action :set_cart, only: [:create, :index]
 
     def index
-        puts "#" * 100
-        puts "salut"
-        puts "#" * 100
-        
-    end
-
-    def show
-        @item = Item.all
     end
 
     def create 
         item = Item.find(params[:format])
         @order.items << item
         redirect_to root_path
-    end
+    end  
 end
     
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ApplicationRecord
     has_many :item_orders
     has_many :items, through: :item_orders
 
+    def total
+        items.sum(&:price)
+    end
 end
