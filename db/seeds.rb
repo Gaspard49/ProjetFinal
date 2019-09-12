f250bda3a37b438e70fdbe6b2972611e9851ea83
BEECECE <be.charrier@yahoo.com>
2019-09-12 10:16:12 +0200

    2 pour le seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,15 +17,14 @@ ItemOrder.delete_all
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65), city: Faker::Address.city, zip_code:  Faker::Address.zip_code, address: Faker::Address.street_address ,password: 'password',)
 end
 
-10.times do
+2.times do
   Item.create(title: Faker::Movie.quote, description: Faker::Lorem.sentence(word_count: 3), price: Faker::Number.decimal(l_digits: 2), category: Faker::Team.name)
 end
 
-10.times do
-  Order.create(user_id: User.all.sample.id)
+2.times do
   Order.create(user_id: User.all.sample.id)
 end
 
-10.times do
+2.times do
   ItemOrder.create(order_id: Order.all.sample.id, item_id: Item.all.sample.id)
 end
