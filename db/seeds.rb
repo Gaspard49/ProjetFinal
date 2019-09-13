ea82ea3bd759fadacf087a13f65d66eed6b173a8
Gaspard49 <gaspard.tertrais@gmail.com>
2019-09-13 16:09:00 +0200

    button admin + seed admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,8 @@ puts "Delete done!"
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65), city: Faker::Address.city, zip_code:  Faker::Address.zip_code, address: Faker::Address.street_address ,password: 'password')
 end
 
+User.create(first_name: 'Admin', last_name: 'Admin', email: 'admin@admin.com',password: 'admin123', admin: true)
+
 5.times do
   Item.create(title: Faker::Movie.quote, description: Faker::Lorem.sentence(word_count: 3), price: Faker::Number.decimal(l_digits: 2), category: Faker::Team.name)
 end
