205125dd65d1bb5dfe0cb38ce6c73d0c88941141
BEECECE <be.charrier@yahoo.com>
2019-09-13 16:02:36 +0200

    false seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ ItemOrder.delete_all
 puts "Delete done!"
 
 2.times do
-  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65), city: Faker::Address.city, zip_code:  Faker::Address.zip_code, address: Faker::Address.street_address ,password: 'password')
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 65), city: Faker::Address.city, zip_code:  Faker::Address.zip_code, address: Faker::Address.street_address ,password: 'password', admin: false)
 end
 
 5.times do
