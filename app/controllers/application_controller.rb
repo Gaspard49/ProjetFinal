d7a2eb1da49f72482448a15141bcf2078684b37d
Gaspard49 <gaspard.tertrais@gmail.com>
2019-09-17 15:44:59 +0200

    modify register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
       def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :date_of_birth, :address, :zip_code, :city, :email, :password, :avatar) }
+          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
           devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :date_of_birth, :address, :zip_code, :city, :email, :current_password, :avatar) }
       end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :pictures, only: [:create]
   end
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
 
   resources :users, only: [:show]
   
