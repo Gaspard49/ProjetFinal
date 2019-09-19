0d4b9c5ce2bb4916178b2f6d3bf8001ba229c2e7
adrien66 <adrien66.pub@gmail.com>
2019-09-19 12:15:26 +0200

    admin mailer fix2

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,7 +4,7 @@ class AdminMailer < ApplicationMailer
     def order_paid(order_paid)
         @order = order_paid 
         @user = User.find(order_paid.user_id)          
-        email_with_name = %("admin4321@yopmail.com")  
+        email_with_name = %("admin43210@yopmail.com")  
         mail(to: email_with_name, subject: 'Order Paid !')
       end
 end
