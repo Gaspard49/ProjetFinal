31969b6e624a936ff64e13e9a4f722b4de168033
adrien66 <adrien66.pub@gmail.com>
2019-09-19 11:23:48 +0200

    avatar, optional true

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :orders
-  has_one_attached :avatar
+  has_one_attached :avatar, optional: true
   validates :email, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
