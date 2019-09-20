804ef2efcc67e7a9ddb9404cbd6ac0ef31403401
Gaspard49 <gaspard.tertrais@gmail.com>
2019-09-20 15:03:56 +0200

    button order

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,7 @@ class Category < ApplicationRecord
   has_many :join_table_category_items
   has_many :items, through: :join_table_category_items
   has_one_attached :picture 
+  validates :name, presence: true
+  validates :picture, presence: true
 end
 
