f48ce4a2ba38f6609035c17523ca17d62d85a5d4
adrien66 <adrien66.pub@gmail.com>
2019-09-21 17:49:46 +0200

    change region aws Oregon

--- config/environment.rb
+++ config/environment.rb
@@ -3,23 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-=begin
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
-  :password => ENV['SENDGRID_PWD'],
-  :domain => 'https://projet-final-caen.herokuapp.com/',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-=end
 
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['AWS_SES_LOGIN'],
   :password => ENV['AWS_SES_PWD'],
   :domain => 'https://projet-final-caen.herokuapp.com/',
-  :address => 'email-smtp.eu-west-1.amazonaws.com',
+  :address => 'email-smtp.us-west-2.amazonaws.com',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
