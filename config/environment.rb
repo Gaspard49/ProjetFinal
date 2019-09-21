081814be70fb57ab83a59e191e0332349ac83f3f
adrien66 <adrien66.pub@gmail.com>
2019-09-21 16:17:26 +0200

    sendrid test

--- config/environment.rb
+++ config/environment.rb
@@ -3,31 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-=begin
+
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
+  :domain => 'https://projet-final-caen.herokuapp.com/',
   :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-https://dashboard.heroku.com/apps/projet-final-caen
-
-
-SMTP username: Your Gmail address
-SMTP password: Your Gmail password
-SMTP server address: smtp.gmail.com
-Gmail SMTP port (TLS): 587
-SMTP port (SSL): 465
-SMTP TLS/SSL required: yes
-=end
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['GMAIL_LOGIN'],
-  :password => ENV['GMAIL_PWD'],
-  :domain => 'gmail.com',
-  :address => 'smtp.gmail.com',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
