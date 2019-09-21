2e8b68967a5b8c15cca978d596a725f88616c436
adrien66 <adrien66.pub@gmail.com>
2019-09-21 18:41:28 +0200

    sendgrig back

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'adrien66.pub@gmail.com'
+  default from: 'from@example.com'
   layout 'mailer'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,6 +5,16 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_LOGIN'],
+  :password => ENV['SENDGRID_PWD'],
+  :domain => 'monsite.fr',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+=begin
+ActionMailer::Base.smtp_settings = {
   :user_name => ENV['AWS_SES_LOGIN'],
   :password => ENV['AWS_SES_PWD'],
   :domain => 'https://projet-final-caen.herokuapp.com/',
@@ -13,3 +23,4 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+=end
