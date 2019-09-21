30a351888e375fb683dfa5f458f01a0fcd558052
adrien66 <adrien66.pub@gmail.com>
2019-09-21 13:48:00 +0200

    environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,23 +3,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-=begin
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_LOGIN'],
-  :password => ENV['SENDGRID_PWD'],
-  :domain => 'monsite.fr',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
-=end
 
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_LOGIN'],
   :password => ENV['SENDGRID_PWD'],
   :domain => 'monsite.fr',
-  :address => "ses-smtp-user.20190921-130833",
+  :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
