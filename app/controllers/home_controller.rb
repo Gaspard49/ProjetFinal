ad8c06ef2e0f2cf140ab9c7ce0c35e3e8fd600c7
adrien66 <adrien66.pub@gmail.com>
2019-09-22 17:39:08 +0200

    fix home_controller delete @item_order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,6 @@ class HomeController < ApplicationController
     
     def index
         @items = Item.all
-        @item_order = ItemOrder.where("order_id = ?", @order.id)
         @subscriber = Subscriber.new
         @categories = Category.all
     end
