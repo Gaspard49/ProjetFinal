85e1994b2ff45ea4954f5798be6be5009343c783
adrien66 <adrien66.pub@gmail.com>
2019-09-22 16:02:50 +0200

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -5,16 +7,16 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_categories
-  
+
   protected
 
-      def configure_permitted_parameters
-          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
-          devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :date_of_birth, :address, :zip_code, :city, :email, :current_password, :avatar) }
-      end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :date_of_birth, :address, :zip_code, :city, :email, :current_password, :avatar) }
+  end
 
-      def set_categories
-        @categories = Category.all
-      end
+  def set_categories
+    @categories = Category.all
+  end
 
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,30 +1,32 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   before_action :find_commentable
-  
-      def new
-        @comment = Comment.new
-      end
-  
-      def create
-        @comment = @commentable.comments.new comment_params
-        @comment.author = current_user.id
-  
-        if @comment.save
-          redirect_back(fallback_location: @item_path)
-        else
-          redirect_back(fallback_location: @item_path)
-        end
-      end
-  
-      private
-  
-      def comment_params
-        params.require(:comment).permit(:body)
-      end
-  
-      def find_commentable
-        @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
-        @commentable = Item.find_by_id(params[:item_id]) if params[:item_id]
-      end
-  
+
+  def new
+    @comment = Comment.new
   end
+
+  def create
+    @comment = @commentable.comments.new comment_params
+    @comment.author = current_user.id
+
+    if @comment.save
+      redirect_back(fallback_location: @item_path)
+    else
+      redirect_back(fallback_location: @item_path)
+    end
+  end
+
+    private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def find_commentable
+    @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
+    @commentable = Item.find_by_id(params[:item_id]) if params[:item_id]
+  end
+  
+end
